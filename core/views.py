6e8c0b4cc9d40a3ba5845ad1514c4a1bde58e919
Farid Karimi <fkarimi8320@gmail.com>
2023-08-11 21:59:52 +0330

    user settings

--- core/views.py
+++ core/views.py
@@ -31,6 +31,8 @@ def signup(request):
                 user.save()
 
                 #log user to setting
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create profile
 
@@ -64,3 +66,30 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+def settings(request):
+    user_profile = profile.objects.get(user = request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+            
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile' : user_profile})
